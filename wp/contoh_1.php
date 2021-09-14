19e569d96703eea37cad16e5956ea00248ccc752
wp2pramudyagilangpamungkas <90603297+pamug@users.noreply.github.com>
2021-09-14 14:31:15 +0700

    Add files via upload

---
+++ wp/contoh_1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh_1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "perkenalkan saya Pramudya Gilang Pamungkas<br>";
+    echo "Saya mahasiswa kelas 19.3A.05";
+    }
+}
